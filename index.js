2e57b1bd4123d036e1f51ed73e92c0dfe38d3cf1
ariel.olin@apiiro.com <ariel0lin@gmail.com>
2022-12-29 23:08:00 +0200

    mongo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import cors from "cors";
 
 const server = { isAdminConnected: false, connectedUsers: {} };
 dotenv.config();
-
+/* 
 try {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -20,7 +20,7 @@ try {
 } catch (error) {
   console.log(error);
 }
-
+ */
 /* const store = new MongoStore({ mongoose: node --inspect index.js }); */
 
 const app = express();
